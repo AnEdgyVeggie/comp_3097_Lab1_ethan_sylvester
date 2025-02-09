c85f2f9afb0bf18bc7883f2dd6473c516e9e8a59
ethan <ethan.sylvester@gmx.com>
2025-02-09 10:44:17 -0500

    initial commit

---
+++ labtest/labtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  labtestApp.swift
+//  labtest
+//
+//  Created by Ethan Sylvester on 2025-02-09.
+//
+
+import SwiftUI
+
+@main
+struct labtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
