8a40b887b1286ef5a890d7427c5194bc01c5b823
ethan <ethan.sylvester@gmx.com>
2025-02-09 11:09:40 -0500

    established layout for application

---
+++ labtestTests/labtestTests.swift
@@ -0,0 +1,17 @@
+//
+//  labtestTests.swift
+//  labtestTests
+//
+//  Created by Ethan Sylvester on 2025-02-09.
+//
+
+import Testing
+@testable import labtest
+
+struct labtestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
